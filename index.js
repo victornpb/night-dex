f9dd4891754d8df3f263691459d9cc213d1a5d36
Victor Nunes <victornunes@lett.com.br>
2018-01-22 19:20:39 -0200

    test

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+app.get('/nightbot-pokemon/:channel/:user/:userLevel/:count/:query', function (req, res) {
+  res.send(" Hello World " + req.params);
+})
