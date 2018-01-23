ae257cb16255349ef9c149e998038d3489d01e7e
Victor Nunes <victornunes@lett.com.br>
2018-01-22 23:04:45 -0200

    dex

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const oakdexPokedex = require('oakdex-pokedex');
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
@@ -15,10 +17,22 @@ app.get('/nightbot-pokemon/:channel/:user/:userLevel/:count/:query', function (r
 })
 
 app.get('/nightbot-pokemon/:channel/:user/:userLevel/:count', function (req, res) {
-  res.send(`⚡💢 @${req.param.user} found a wild Bulbasaur❗️\n🔴 https://pokemondb.net/pokedex/bulbasaur`);
+  res.send(`⚡💢 @${req.params.user} found a wild Bulbasaur❗️\n🔴 https://pokemondb.net/pokedex/bulbasaur`);
 })
 
 
-app.get('/dex', function (req, res) {
-  res.send(JSON.stringify(req.query));
+// app.get('/dex', function (req, res) {
+//   res.send(JSON.stringify(req.query));
+
+
+
+// })
+
+app.get('/dex/:query', function (req, res) {
+
+  oakdexPokedex.findPokemon(req.params.query, function (p) {
+    console.log(p.names); // Eeevee 
+  
+    res.send(JSON.stringify(p));
+  });
 })
