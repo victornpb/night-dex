b92c9b4e5529029369ce0463138e178fd045c2a4
Victor Nunes <victornunes@lett.com.br>
2018-01-25 18:16:09 -0200

    minor formatting tweaks

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-// 
+// Pokedex imports
 const Dex = require('./dex');
 const unifont = require('./unifont');
 const eastereggs = require('./eastereggs');
 
-const dex = new Dex(); //instance
+
 
 Array.prototype.random = function () {
     return this[Math.floor((Math.random() * this.length))];
-}
+};
 
 const app = express();
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const dex = new Dex(); //instance
 
 // app.use(express.static(path.join(__dirname, 'public')))
 // app.set('views', path.join(__dirname, 'views'))
@@ -25,43 +26,60 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 // app.get('/', (req, res) => res.render('pages/index'))
 
 const CMD = '!dex';
-const PREFIX = '📟 🄳🄴🅇 ';
+const PREFIX = unifont('📟 DEX ', 'squaredneg');
 
 app.get('/dex', function (req, res) {
-
-    let out = `Instruction: ${CMD} help. `;
+    let out = "";
+    // out += `Instruction: ${unifont(CMD + ' help', 'sansbold')}. `;
+    out += unifont(`🎲🎲 RANDOM 🎲🎲`, 'boldscript');
     let p = dex.getRandom();
     out += printPokemon(p);
-    res.send(PREFIX+out);
+
+    res.set({
+        'content-type': 'text/plain; charset=utf-8'
+    });
+    res.send(PREFIX + out);
 });
 
 app.get('/dex/help', function (req, res) {
-    let out = `Type ${CMD} POKEMON or NUMBER. Type ${CMD} to get a random Pokemon.`;
+    let out = '';
+    out += `${unifont('HELP', 'squared')}❓`;
+    out += ` ➤ ${unifont(CMD, 'sansbold')} to get a random Pokemon.`;
+    out += ` ➤ ${unifont(CMD + ' gengar', 'sansbold')} or ${unifont(CMD + ' 94', 'sansbold')} to see the Pokedex info about Gengar.`;
+    out += ` ➤ ${unifont(CMD + ' about', 'sansbold')} to see about this command.`;
+
+    res.set({
+        'content-type': 'text/plain; charset=utf-8'
+    });
     res.send(PREFIX + out);
 });
 
 app.get('/dex/:q', function (req, res) {
     let out;
     var q = String(req.params.q).trim();
-    
+
     let ee = eastereggs(q);
     if (ee) {
         out = ee;
-    }
-    else { 
+    } else {
 
         let p = dex.find(q)[0];
         if (p) {
             out = printPokemon(p);
-        }
-        else {
+        } else { //not found
+            out = unifont(`🕵 This Pokemon is not on the database!`, 'sansbold') + ` (${q})`;
             let suggestions = dex.suggestions(q);
-            out = `🕵 This Pokémon is not on the database! ` + (suggestions.length ? `🔮 But you can try ${suggestions.length > 1 ?'one of those':'that one'}! ` + suggestions.map(p => `➤ ${CMD} ${p.names.en}`).join(' '):'');
+            if (suggestions.length) {
+                out += ` 🔮 But you can try ${suggestions.length > 1 ? 'one of these' : 'this one'}: `;
+                out += suggestions.map(p => `🔹${CMD} ${p.names.en}`).join(' ');
+            }
         }
     }
 
 
-    res.set({ 'content-type': 'text/plain; charset=utf-8' })
+    res.set({
+        'content-type': 'text/plain; charset=utf-8'
+    });
     res.send(limit(PREFIX + out));
 
 });
@@ -69,7 +87,7 @@ app.get('/dex/:q', function (req, res) {
 
 function limit(str) {
     if (str.length >= 400) {
-        return str.substring(0, 400-1)+'…';
+        return str.substring(0, 400 - 1) + '…';
     }
     return str;
 }
@@ -87,8 +105,8 @@ function printPokemon(p) {
     };
 
     const name = `【#${p.national_id} ${p.names.en.toUpperCase()}】`;
-    const type = unifont('🎲TYPE:', 'bold') + p.types.join(' ');
-    const abilities = unifont('🎓ABILITIES:','bold') + p.abilities.map(a => a.name + (p.hidden ? '*' : '')).join(', ');
+    const type = unifont('🔷TYPE:', 'bold') + p.types.join(' ');
+    const abilities = unifont('🎓ABILITIES:', 'bold') + p.abilities.map(a => a.name + (p.hidden ? '*' : '')).join(', ');
     const base_stats = unifont('📊 BASE:', 'bold') + Object.keys(p.base_stats).map(a => `${unifont(ABREV[a], 'normal')}${p.base_stats[a]}`).join('|');
     const ev_yield = unifont('🔸EVYIELD:', 'bold') + Object.keys(p.ev_yield).map(a => `${unifont(ABREV[a], 'normal')}${p.ev_yield[a]}`).join('|');
     const link = `pokemondb.net/pokedex/${p.names.en}`;
@@ -104,7 +122,6 @@ function printPokemon(p) {
         // link,
         quote,
     ].join(' ');
-
 
     return out;
 }
