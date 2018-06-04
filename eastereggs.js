de362e601c93c1f362877000c57ee6c0a42c4f31
victornpb <victornpb@gmail.com>
2018-06-03 22:26:13 -0300

    accept @user as regulars

--- eastereggs.js
+++ eastereggs.js
@@ -9,19 +9,19 @@ module.exports = function (q) {
 };
 
 function testRegulars(str) {
-    const q = String(str).toLowerCase();
+    const q = String(str).toLowerCase().replace('@','');
 
     const person = Object.keys(regulars).find(k => {
         return k.toLowerCase() === q ? k : null;
     });
 
-    if (person) return ` 🎁【 @ ${person} 】 ${regulars[person].random()}`;
+    if (person) return ` 🎁【 @${person} 】 ${regulars[person].random()}`;
 }
 
 
 
 function testCredits(str) {
-    const q = String(str).toLowerCase().trim();
+    const q = String(str).toLowerCase().trim().replace('@','');
     
     if (q.match(/victor(npb)?|about/)) {
         return [
